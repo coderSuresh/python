ce2e4d5d5ea001552e713b0c7da17bfa3c2e62cc
Paras Bhandari <50918320+gamedev-parash@users.noreply.github.com>
2022-09-30 11:11:06 +0545

    Day1

---
+++ Day1/GenarateBandName.py
@@ -0,0 +1,4 @@
+name_city=len(input("What's name og the city you grew up in?\n"))
+fav_animal=input("What's name og the city you grew up in?\n")
+print("Your Band name could be "+name_city+" "+fav_animal)
+print(type(name_city))
---
+++ Day1/inputFunction.py
@@ -0,0 +1,3 @@
+s=input("What is your name? ")
+print(len(s))
+ 
